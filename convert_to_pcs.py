73950f463a2377228285cf4606c5223bdf2538bf
olegzaikin <oleg.zaikin@icc.ru>
2022-12-27 16:07:06 +0800

    Discretization.

--- convert_to_pcs.py
+++ convert_to_pcs.py
@@ -11,7 +11,7 @@
 #==============================================================================
 
 script_name = "convert_to_pcs.py"
-version = '0.0.6'
+version = '0.0.7'
 
 import sys
 
@@ -27,16 +27,16 @@ def read_solver_parameters(param_file_name : str):
   with open(param_file_name, 'r') as param_file:
     lines = param_file.read().splitlines()
     for line in lines:
-      if 'seed' in line or 'statistics' in line or 'verbose' or 'quiet' in line:
+      if 'seed' in line or 'statistics' in line or 'verbose' in line or 'quiet' in line:
         continue
       words = line.strip().split(' ')
       # kissat --range format:
       if len(words) == 4 and '=' not in line:
           p = Param()
           p.name = words[0]
-          p.left_bound = words[1]
-          p.default = words[2]
-          p.right_bound = words[3] 
+          p.left_bound = int(words[1])
+          p.default = int(words[2])
+          p.right_bound = int(words[3]) 
       # cadical --help format:
       elif len(words) > 1 and '=' in words[0]:
           assert('--' in words[0])
@@ -45,8 +45,8 @@ def read_solver_parameters(param_file_name : str):
           p = Param()
           p.name = s.split('=')[0]
           if s.split('=')[1] == 'bool':
-              p.left_bound = '0'
-              p.right_bound = '1'
+              p.left_bound = 0
+              p.right_bound = 1
           else:
               p.left_bound = s.split('=')[1].split('..')[0]
               if 'e' in p.left_bound:
@@ -61,36 +61,78 @@ def read_solver_parameters(param_file_name : str):
               p.default = 0
           elif 'e' in p.default:
               p.default = str(int(float(p.default)))
+      print(words)
+      assert(isinstance(p.left_bound, int))
+      assert(isinstance(p.right_bound, int))
+      assert(isinstance(p.default, int))
+      assert(p.left_bound < p.right_bound)
+      assert(p.right_bound > 0)
       params.append(p)
   assert(len(params) > 0)
   return params
 
+# Convert to string a given list of values:
+def domain_to_str(name : str, default : int, values : list):
+  assert(name != '')
+  assert(len(values) > 0)
+  assert(default in values)
+  s = name + ' {'
+  for i in values:
+    s += str(i)
+    if i != values[-1]:
+      s += ', '
+  s += '}[' + str(default) + ']'
+  return s
+
 if __name__ == '__main__':
 
   if len(sys.argv) == 1:
-    sys.exit('Usage : ' + script_name + ' solver-parameters')
+    sys.exit('Usage : ' + script_name + ' solver-parameters-file')
 
   param_file_name = sys.argv[1]
   params = read_solver_parameters(param_file_name)
-  print(str(len(params)) + ' params')
+  #print(str(len(params)) + ' params')
+
+  values_num = 0
 
   # Convert each parameter to the PCS format:
   pcs_str = ''
   for p in params:
     # If Boolean:
-    if p.left_bound == '0' and p.right_bound == '1':
-      default_bool = 'true' if p.default == '1' else 'false'
+    if p.left_bound == 0 and p.right_bound == 1:
+      default_bool = 'true' if p.default == 1 else 'false'
       pcs_str += p.name + ' {false, true}[' + default_bool + ']'
-    # If integer with too many values, force logariphmic steps:
-    elif float(p.left_bound) > 0 and int(p.right_bound) - int(p.left_bound) > 100 and float(p.default) > 0:
-        lb = '1' if int(p.left_bound) == 0 else p.left_bound
-        pcs_str += p.name + ' [' + lb + ', ' + p.right_bound + ']' +\
-        '[' + p.default + ']il'
-    # Just integer:
+      values_num += 2
+    # If integer with few values:
+    elif int(p.right_bound) - int(p.left_bound) < 10: # e.g. [0,9] or [11,20]
+      values = [i for i in range(p.left_bound, p.right_bound + 1)]
+      pcs_str += domain_to_str(p.name, p.default, values)
+      values_num += len(values)
+    # If integer with too many values, use logariphmic steps:
     else:
-      pcs_str += p.name + ' [' + p.left_bound + ', ' + p.right_bound + ']' +\
-      '[' + p.default + ']i'
+      koef = 2 if int(p.right_bound) - int(p.left_bound) <= 100000 else 4
+      values = [p.left_bound]
+      i = p.left_bound
+      while values[-1] != p.right_bound:
+        if i < 0:
+          i *= -1 / koef
+        elif i == 0:
+          i = 1
+        else:
+          i *= koef
+        if i >= p.right_bound:
+          values.append(p.right_bound)
+          break
+        values.append(i)
+      if p.default not in values:
+        values.append(p.default)
+      values = sorted(values)
+      pcs_str += domain_to_str(p.name, p.default, values)
+      values_num += len(values)
     pcs_str += '\n'
+
+  print('')
+  print(str(values_num) + ' values in ' + str(len(params)) + ' domains')
 
   # Report results:
   pcs_file_name = param_file_name + '.pcs'
