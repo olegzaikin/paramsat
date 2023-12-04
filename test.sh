1f3571c52f3741ad503816515acf56714ee51f2c
olegzaikin <oleg.zaikin@icc.ru>
2023-12-04 13:00:34 +0800

    Don't ignore *.pcs.

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 !/usr/bin/bash
 
-python3 ./bbo_param_solver.py ./kissat3 ./kissat3_md_given.pcs ./cnfs_supereasy/ -maxpoints=2 -cpunum=1
-python3 ./bbo_param_solver.py ./kissat3 ./kissat3_md_given.pcs ./cnfs_supereasy/ -maxpoints=5 -cpunum=5
-python3 ./bbo_param_solver.py ./kissat3 ./kissat3_md_given.pcs ./cnfs_supereasy/ -maxpoints=10 -cpunum=3
+python3 ./bbo_param_solver.py ./kissat3 ./kissat3-md-given.pcs ./cnfs_easy/ -maxpoints=2 -cpunum=1
+python3 ./bbo_param_solver.py ./kissat3 ./kissat3-md-given.pcs ./cnfs_easy/ -maxpoints=5 -cpunum=5
+python3 ./bbo_param_solver.py ./kissat3 ./kissat3-md-given.pcs ./cnfs_easy/ -maxpoints=10 -cpunum=3
